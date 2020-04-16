c9eda8d72fc555f8bd474ebfa0899e8ead49b3d5
David Kenyi <52720963+johnnyalier@users.noreply.github.com>
2020-04-15 21:20:59 -0400

    Update browsebooks.js

--- Project/pages/browsebooks.js
+++ Project/pages/browsebooks.js
@@ -2,7 +2,6 @@ let isbn;
 
 async function loadBooks(){
   try{
-    console.log("Connected and books loaded");
     var result = await fetch("http://localhost:3000/getBooks", {method:"GET"});
     let books   = await result.json();
 
@@ -32,7 +31,6 @@ async function loadBooks(){
 }
 
 async function addToOrders(checked, isbn, price){
-  console.log("addToOrders: " + checked + " and isbn = " + isbn + "Price " + price);
   if (checked){
     let result = await fetch("http://localhost:3000/addToOrders",
     {
@@ -56,9 +54,8 @@ async function viewOrders(){
     let viewOrders       = document.getElementById("viewOrders");
     viewOrders.innerHTML = ""
 
-    // we are adding the books the user has in the cart currently to be displayed on the screen if there was a success on the server
+    // Each book the user checks is added to the orders relation
     if (booksInCart.success){
-      // this object has the u_id of the specified user
       for (b of booksInCart.result) {
         var breakLine = document.createElement("br");
         var book      = document.createElement("input");
@@ -104,16 +101,12 @@ async function updateQuantity(isbn){
   let viewOrders = document.getElementById("viewOrders");
   let counter = 0;
 
-  // get all the children
   let children = viewOrders.childNodes;
-
-  // we are looking for the INPUT tags and the one that have a the isbn of the book that we are looking for exactly
   for (child of children){
     if (child.tagName == "INPUT" && child.id == isbn){
       counter ++;
     }
   }
-  console.log("counter: " + counter + " isbn " + isbn);
   // make a POST request to the server to update the values for the quntity on the books on the server
   await fetch("http://localhost:3000/updateQuantity",
   {
